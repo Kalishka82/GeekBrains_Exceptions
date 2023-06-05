df0a443af7fcfbb34fc4d9faf23463a98aeec05b
Tatiana Kalinina <tankakalinka@gmail.com>
2023-06-05 15:57:10 +0300

    changes in ex_01

--- src/main/java/web_01_ht_Exceptions/ex_01.java
+++ src/main/java/web_01_ht_Exceptions/ex_01.java
@@ -6,39 +6,32 @@ public class ex_01 {
         int b = 0;
 //        System.out.println(divide(a, b));
 
-        int[] arr = {1, 2, 3};
-        int min_length = 6;
-//        if (! checkArrayLength(arr, min_length)) {
-//            System.out.println("Длина заданного массива меньше минимальной");
-//        }
+        int[] arr = {1, 2, 3, 4, 5};
+        int index = 8;
+//        System.out.println(findValue(arr, index));
 
-        int[][] matrix = {{1, 2, 3, 4}, {4, 5, 6, 7}, {7, 8, 9, 10}};
-        System.out.println(sumSquareMatrix(matrix));
+        String str = "Hello, world!";
+        System.out.println(num(str));
     }
 
     public static int divide(int a, int b) {
-        if (b == 0) {
-            throw new RuntimeException("На ноль делить нельзя");
-        }
         return a / b;
     }
 
-    public static boolean checkArrayLength(int[] arr, int min_length) {
-        if (arr.length < min_length) {
-            return false;
-        }
-        return true;
+    public static int findValue (int[] arr, int index) {
+        return arr[index];
     }
 
-    public static int sumSquareMatrix(int[][] matrix) {
+    public static int num(String str) {
+        int number = Integer.parseInt(str);
+        return number;
+    }
+
+    public static int sumMatrix(int[][] matrix) {
         int sum = 0;
-        if (matrix.length != matrix[1].length) {
-            return -1;
-        } else {
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix.length; j++) {
-                    sum += matrix[i][j];
-                }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j <= matrix[1].length; j++) {
+                sum += matrix[i][j];
             }
         }
         return sum;
